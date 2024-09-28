9f290efb3f77e2405d0204b8b0dd08fd8fb25690
goshva <sha.egor@gmail.com>
2024-09-28 08:18:27 +0300

    add decode url example

--- js/genRoute.js
+++ js/genRoute.js
@@ -12,6 +12,7 @@ function saveCoordinates(card) {
 function redirectYandexRoute() {
   // Redirect to yandex route 
   // https://yandex.ru/maps/11062/kislovodsk/?ll=42.714506%2C43.896928&mode=routes&rtext=43.897347%2C42.711667~43.895456%2C42.714317~43.894118%2C42.711227&rtt=bc&ruri=~~ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgo0MTQwMTc4MzU2EmnQoNC-0YHRgdC40Y8sINCh0YLQsNCy0YDQvtC_0L7Qu9GM0YHQutC40Lkg0LrRgNCw0LksINCa0LjRgdC70L7QstC-0LTRgdC6LCDRg9C70LjRhtCwINCa0LDRgtGL0YXQuNC90LAsIDIiCg1L2CpCFZSTL0I%2C&z=15.55
+  // decode: https://yandex.ru/maps/11062/kislovodsk/?ll=42.714506,43.896928&mode=routes&rtext=43.897347,42.711667~43.895456,42.714317~43.894118,42.711227&rtt=bc&ruri=ymapsbm1://geo?data=IgoNv9gqQhXili9C~~ymapsbm1://geo?data=Cgo0MTQwMTc4MzU2EmnQoNC-0YHRgdC40Y8sINCh0YLQsNCy0YDQvtC_0L7Qu9GM0YHQutC40Lkg0LrRgNCw0LksINCa0LjRgdC70L7QstC-0LTRgdC6LCDRg9C70LjRhtCwINCa0LDRgtGL0YXQuNC90LAsIDIiCg1L2CpCFZSTL0I,&z=15
   var savedCoordinates = localStorage.getItem('savedCoordinates');
   if (savedCoordinates) {
     var coordinates = JSON.parse(savedCoordinates);
