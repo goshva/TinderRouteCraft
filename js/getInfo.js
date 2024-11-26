3a0cf8a5581fedcc6e671aee363bb1d68d215bba
goshva <sha.egor@gmail.com>
2024-11-26 10:00:08 +0300

    add point source

--- js/getInfo.js
+++ js/getInfo.js
@@ -4,22 +4,28 @@ function setFeatures(features) {
 };
 async function loadFeatures() {
     try {
-        const response = await fetch('./api/map.json');
+//      const response = await fetch('./api/map.json');
+        const response = await fetch('http://c92834bq.beget.tech/map.json',{
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+            },
+        });
         const data = await response.json();
-        //this.setFeatures(data.config?.userMap?.features);
         createCards(data.config?.userMap?.features);
     } catch (error) {
         console.error('Failed to load features:', error);
     }
 };
 function createCard(object) {
+    console.warn(object);
     const card = document.createElement('div');
     card.className = 'tinder--card';
     //card.dataset.id = object.id;
     card.dataset.coordinates = [object.coordinates[1],object.coordinates[0]];
 
     const img = document.createElement('img');
-    img.src = object.imageUrl || './img/default-image.png';  // Use a default image if none is provided
+    img.src = object.imageUrl || './img/default-image.png';
     card.appendChild(img);
 
     const title = document.createElement('h4');
